7c707cd80608bf8759f51e4f00402fd1c4818469
tlneondo <tlneondo@gmail.com>
2025-09-06 15:26:54 -0400

    initial outline

---
+++ mergevdf.py
@@ -0,0 +1,19 @@
+#take windows shortcusts vdf file
+
+
+#parse into python structures
+
+
+#Set launch executable to winwalkawayscript
+
+
+
+
+#insert entries into linux shortcuts vdf
+
+
+
+#pull art and apply
+
+
+#pull favorite & categories and apply
